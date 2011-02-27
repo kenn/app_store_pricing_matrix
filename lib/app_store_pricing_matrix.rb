9934f415607ef3035ee4f6e0da35fbdb8b514f5e
Kenn Ejima <kenn.ejima@gmail.com>
2011-02-27 10:38:15 -0800

    freeze everything

--- lib/app_store_pricing_matrix.rb
+++ lib/app_store_pricing_matrix.rb
@@ -1,14 +1,14 @@
 module AppStorePricingMatrix
   CURRENCY_MAP = {
-    :usd => [ :usd, :mxn ], # WARNING: MXN is a valid Mexican peso - there's no MXP, apparently.
-    :cad => [ :cad ],
-    :aud => [ :aud, :nzd ],
-    :jpy => [ :jpy ],
-    :eur => [ :eur, :dkk, :nok, :sek, :chf ],
-    :gbp => [ :gbp ]
-  }
-  CUSTOMER_CURRENCIES = CURRENCY_MAP.values.flatten.map{|i| i.to_s.upcase }
-  DEVELOPER_CURRENCIES = CURRENCY_MAP.keys.map{|i| i.to_s.upcase }
+    :usd => [ :usd, :mxn ].freeze, # WARNING: MXN is a valid Mexican peso - there's no MXP, apparently.
+    :cad => [ :cad ].freeze,
+    :aud => [ :aud, :nzd ].freeze,
+    :jpy => [ :jpy ].freeze,
+    :eur => [ :eur, :dkk, :nok, :sek, :chf ].freeze,
+    :gbp => [ :gbp ].freeze
+  }.freeze
+  CUSTOMER_CURRENCIES = CURRENCY_MAP.values.flatten.map{|i| i.to_s.upcase }.freeze
+  DEVELOPER_CURRENCIES = CURRENCY_MAP.keys.map{|i| i.to_s.upcase }.freeze
 
   REVERSE_CURRENCY_MAP = {}.tap do |hash|
     CURRENCY_MAP.keys.each do |key|
@@ -16,17 +16,17 @@ module AppStorePricingMatrix
         hash[customer_currency.to_s.upcase] = key.to_s.upcase
       end
     end
-  end
+  end.freeze
 
   CUSTOMER_PRICES = {}.tap do |hash|
     CUSTOMER_CURRENCIES.map do |currency|
-      hash[currency] = File.read("#{File.dirname(__FILE__)}/prices/#{currency}").split("\n")
+      hash[currency] = File.read("#{File.dirname(__FILE__)}/prices/#{currency}").split("\n").freeze
     end
-  end
+  end.freeze
 
   DEVELOPER_PROCEEDS = {}.tap do |hash|
     DEVELOPER_CURRENCIES.each do |key|
-      hash[key] = File.read("#{File.dirname(__FILE__)}/prices/#{key}_pro").split("\n")
+      hash[key] = File.read("#{File.dirname(__FILE__)}/prices/#{key}_pro").split("\n").freeze
     end
-  end
+  end.freeze
 end
