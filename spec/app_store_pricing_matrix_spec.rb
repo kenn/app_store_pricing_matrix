80cff7866248c89e382d81bcb8b155b4897abd3a
Kenn Ejima <kenn.ejima@gmail.com>
2014-08-19 17:07:18 -0700

    RSpec 3

--- spec/app_store_pricing_matrix_spec.rb
+++ spec/app_store_pricing_matrix_spec.rb
@@ -4,30 +4,30 @@ module ExampleMethods
   def spec_meat(file, currency)
     context "Currency: #{currency}" do
       it "should have currency table" do
-        File.exists?(file).should == true
+        expect(File.exists?(file)).to eq(true)
       end
 
       File.open(file) do |text|
         lines = text.readlines
 
         it "should have 0 to 87 tiers" do
-          lines.size.should be(88)
+          expect(lines.size).to be(88)
         end
 
         it "should have 0 for tier 0" do
-          ['0', '0.00'].should include lines.first.chomp
+          expect(['0', '0.00']).to include lines.first.chomp
         end
 
         it "should verify format of table content" do
           lines.each_with_index do |line,i|
-            line.should match(/^\d+(?:\.\d\d)?\n?$/), "Error while reading \"#{File.basename(file)}\", line:#{i+1}"
+            expect(line).to match(/^\d+(?:\.\d\d)?\n?$/), "Error while reading \"#{File.basename(file)}\", line:#{i+1}"
           end
         end
         
         it "should have increasing prices" do
           lines.each_with_index do |line,i|
             next if i == 0
-            line.to_f.should be > lines[i - 1].to_f 
+            expect(line.to_f).to be > lines[i - 1].to_f 
           end
         end
       end
@@ -55,25 +55,25 @@ describe "AppStorePricingMatrix" do
   context "Currency Mapping" do
     it "should return the given currency code if it is a valid customer currency" do
       AppStorePricingMatrix::CURRENCIES.each do |currency|
-        AppStorePricingMatrix.customer_currency_for(currency).should == currency
+        expect(AppStorePricingMatrix.customer_currency_for(currency)).to eq(currency)
       end
     end
     
     it "should return EUR for any currency codes that operate the store in Euros" do
       AppStorePricingMatrix::EURO_CURRENCIES.each do |currency|
-        AppStorePricingMatrix.customer_currency_for(currency).should == "EUR"
+        expect(AppStorePricingMatrix.customer_currency_for(currency)).to eq("EUR")
       end
     end
     
     it "should return nil for any unknown currency code" do
-      AppStorePricingMatrix.customer_currency_for(nil).should == nil
-      AppStorePricingMatrix.customer_currency_for("XXX").should == nil
-      AppStorePricingMatrix.customer_currency_for("-").should == nil
+      expect(AppStorePricingMatrix.customer_currency_for(nil)).to eq(nil)
+      expect(AppStorePricingMatrix.customer_currency_for("XXX")).to eq(nil)
+      expect(AppStorePricingMatrix.customer_currency_for("-")).to eq(nil)
     end
     
     it "should accept symbols and lowercase currency codes" do
-      AppStorePricingMatrix.customer_currency_for(:eur).should == "EUR"
-      AppStorePricingMatrix.customer_currency_for('usd').should == "USD"
+      expect(AppStorePricingMatrix.customer_currency_for(:eur)).to eq("EUR")
+      expect(AppStorePricingMatrix.customer_currency_for('usd')).to eq("USD")
     end
   end
 end
